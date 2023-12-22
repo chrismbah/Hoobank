eab2b0bf170b6977eda2016dd07e998d18aaf0ac
Chris Mbah <mbahchris46@gmail.com>
2023-12-22 21:34:32 +0100

    Minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ export default function Hero() {
         <img
           src={robot}
           alt="billing"
-          className="w-[100%] h-[100%] relative z-[5]"
+          className="w-[80%] h-[90%] relative z-[5]"
         />
 
         {/* gradient start */}
